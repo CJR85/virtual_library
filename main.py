df1cbf25e3e91d867aa11f46ce07514a831c93e2
Calvin Roberts <roberts.calvin85@gmail.com>
2023-07-03 23:56:34 +0100

    Build SQLite Database into the Flask website

--- main.py
+++ main.py
@@ -1,27 +1,48 @@
 from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-
-all_books = []
-
+##CREATE DATABASE
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///books.db"
+# Create the extension
+db = SQLAlchemy()
+# initialise the app with the extension
+db.init_app(app)
 
 @app.route('/')
 def home():
+    ##READ ALL RECORDS
+    # Construct a query to select from the database. Returns the rows in the database
+    result = db.session.execute(db.select(Book).order_by(Book.title))
+    # Use .scalars() to get the elements rather than entire rows from the database
+    all_books = result.scalars()
     return render_template("index.html", books=all_books)
 
 
-@app.route("/add", methods=['GET', 'POST'])
+##CREATE TABLE
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(250), unique=True, nullable=False)
+    author = db.Column(db.String(250), nullable=False)
+    rating = db.Column(db.Float, nullable=False)
+
+# Create table schema in the database. Requires application context.
+with app.app_context():
+    db.create_all()
+
+
+@app.route("/add", methods=["GET", "POST"])
 def add():
-    if request.method == 'POST':
-        new_book = {
-            "title": request.form["title"],
-            "author": request.form["author"],
-            "rating": request.form["rating"],
-        }
-        all_books.append(new_book)
-        
+    if request.method == "POST":
+        # CREATE RECORD
+        new_book = Book(
+            title=request.form["title"],
+            author=request.form["author"],
+            rating=request.form["rating"]
+        )
+        db.session.add(new_book)
+        db.session.commit()
         return redirect(url_for('home'))
-    
     return render_template("add.html")
 
 
